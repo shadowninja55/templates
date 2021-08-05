6ff9751da0363326bf83bb649160522c3a1a4666
shadowninja55 <shadowninja1050@gmail.com>
2021-08-05 13:24:37 -0400

    remove cargo stuff

--- python/flake.nix
+++ python/flake.nix
@@ -1,11 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }: 
-    flake-utils.lib.eachDefaultSystem (system:
+    utils.lib.eachDefaultSystem (system:
       let 
         pkgs = nixpkgs.legacyPackages."${system}";
       in {
--- rust/flake.nix
+++ rust/flake.nix
@@ -1,12 +1,12 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nmattia/naersk";
   };
 
   outputs = { self, nixpkgs, flake-utils, naersk, ... } @inputs:
-    flake-utils.lib.eachDefaultSystem (system:
+    utils.lib.eachDefaultSystem (system:
       let 
         pkgs = nixpkgs.legacyPackages."${system}";
         naersk-lib = naersk.lib."${system}";      
