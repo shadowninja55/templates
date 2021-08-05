c1ee02d1e5d5852f2212d73fafe8136eec0d6966
shadowninja55 <shadowninja1050@gmail.com>
2021-08-05 14:07:49 -0400

    use poetry2nix

--- python/flake.nix
+++ python/flake.nix
@@ -2,20 +2,32 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
+    poetry2nix-src.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: 
+  outputs = { self, nixpkgs, utils, poetry2nix-src, ... } @inputs: 
     utils.lib.eachDefaultSystem (system:
       let 
-        pkgs = nixpkgs.legacyPackages."${system}";
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            (pkgs.python39.withPackages (pyPkgs: with pyPkgs; [
-
-            ]))
-          ]; 
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ poetry2nix-src.overlay ];
         };
+        poetry2nix = pkgs.poetry2nix;
+      in rec {
+        defaultPackage = poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+        };
+        defaultApp = utils.lib.mkApp {
+          drv = defaultPackage;
+        };
+        devShell = (poetry2nix.mkPoetryEnv {
+          projectDir = ./.;
+        }).env.overrideAttrs (old: {
+          nativeBuildInputs = old.nativeBuildInputs or [ ] ++ (with pkgs; [
+            poetry
+          ]);
+        });
       }
     );
 }
+
